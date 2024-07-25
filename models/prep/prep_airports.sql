bd6bee2647bf8316acf304dba1d25b69c7fc422b
Pei Huang <huang@live.de>
2024-07-25 10:09:19 +0200

    adding prep airports and flights

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,13 @@
+WITH airports_reorder AS (
+    SELECT  faa
+            ,name
+            ,city
+            ,country
+            ,lat
+            ,lon
+            ,alt
+            ,tz
+            ,dst
+    FROM {{ref('staging_airports')}}
+)
+SELECT * FROM airports_reorder
